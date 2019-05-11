2f46913b4073a50b8acd44c059039e74c99644f4
bauassr <bauassr@gmail.com>
2019-05-11 17:05:27 +0530

    Add files via upload

---
+++ AppAuthorization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+
+namespace WebApplication1
+{
+    public class AppAuthorization : OAuthAuthorizationServerProvider
+    {
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+
+            context.Validated(); // Client is validation 
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            var idententy = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            if(context.UserName =="admin"&& context.Password=="admin")
+            {
+                idententy.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                idententy.AddClaim(new Claim("username", "admin "));
+                idententy.AddClaim(new Claim(ClaimTypes.Name, "Shivam Singh"));
+                context.Validated(idententy);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                idententy.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                idententy.AddClaim(new Claim("username", "user"));
+                idententy.AddClaim(new Claim(ClaimTypes.Name, "Shivam Singh1"));
+                context.Validated(idententy);
+            }
+            else
+            {
+                context.SetError("Invalid_grant", "Provided username and password is incorrect");
+                return;
+            }
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //enable cors origin requests 
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            var AuthProvider = new AppAuthorization();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = AuthProvider
+            };
+
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
